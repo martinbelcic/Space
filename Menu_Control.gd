d8b2fc38b40f8086c942645bbf5173775016b0d4
Wenceslao <wenceslaomateos@gmail.com>
2019-05-02 10:19:46 -0300

    Eliminación de comentarios innecesarios

--- Menu_Control.gd
+++ Menu_Control.gd
@@ -1,9 +1,5 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -20,7 +16,6 @@ func _on_Button_Ships_pressed():
 
 func _on_Button_Start_pressed():
 	get_node("Map_Control").show()
-
 
 func _on_Button_Buy_pressed():
 	pass # Replace with function body.
