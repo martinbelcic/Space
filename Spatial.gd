9102acdedac7a70c57f42847bc4365f390a02988
Wenceslao <wenceslaomateos@gmail.com>
2019-05-07 13:18:01 -0300

    Nave como escena separada

--- Spatial.gd
+++ Spatial.gd
@@ -6,16 +6,18 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#get_node("ShipBody").rotate(Vector3(0,0,0))
 	pass # Replace with function body.
 
 func _process(delta):
-	
 	var ship = get_node("ShipBody")
+	var canvas = get_node("ShipBody/CanvasShip/AnimationPlayer")
 	var x = 0
 	var y = 0
 	var sensitivity = 2
 	if Input.is_action_pressed("ui_left"):
 		x = -sensitivity
+		canvas.play("MoveLeft")
 	if Input.is_action_pressed("ui_right"):
 		x = sensitivity
 	if Input.is_action_pressed("ui_up"):
